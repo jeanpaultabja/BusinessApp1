7bca09d16d9d17c99f489cb6834ce6e0f37bcb49
Jean Paul Tabja <jptabja@hotmail.com>
2020-04-12 16:17:45 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BusinessApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
